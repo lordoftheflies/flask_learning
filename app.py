54afe75ae8abfaeb79fe88402c9022e7d12eeaa6
roozbeh sharifnasab <rsharifnasab@gmail.com>
2019-07-07 15:22:44 +0430

    changed routing from anootation to add url rule , nothing changed in output

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
 def root():
     """
     this method will be called in case of calling localhost
@@ -10,13 +9,19 @@ def root():
     print("root called")
     return "hello from root"
 
-@app.route("/do")
 def second():
     """
     this method will be called in case of calling localhost/do
     """
     print("second method called")
     return "hello from second method"
+
+app.add_url_rule("/","root",root)
+app.add_url_rule("/do","second",second)
+"""
+    now we changed rules from annotation to here
+    we will call add_url_rule method
+"""
 
 
 if (__name__ == '__main__'):
